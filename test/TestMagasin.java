4141a23c657c328105c006fa819d8eb82efe6485
Drouvot Melody <melody.drouvot@gmail.com>
2024-05-15 10:46:24 +0200

    salut

--- test/TestMagasin.java
+++ test/TestMagasin.java
@@ -1,29 +1,28 @@
 import XML.ChargeurMagasin;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestMagasin {
+    @Test
     public void test_chargerMagasin(){
         boolean exception = false;
         try{
-            ChargeurMagasin cm = new ChargeurMagasin("musicbrainzSimple");
+            ChargeurMagasin cm = new ChargeurMagasin("../src/musicbrainzSimple/");
             cm.chargerMagasin();
-        }catch (FileNotFoundException e){
+        }catch (Exception e){
             exception = true;
         }
         assertEquals(false,exception,"Aucune exception devrait etre levée");
     }
 
+    @Test
     public void test_repInexistantChargerMagasin(){
         boolean exception = false;
         try{
             ChargeurMagasin cm = new ChargeurMagasin("poulet");
             cm.chargerMagasin();
-        }catch (FileNotFoundException e){
+        }catch (Exception e){
             exception = true;
         }
         assertEquals(true, exception, "une exception doit etre levée");
